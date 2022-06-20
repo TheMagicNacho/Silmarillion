2a95001aee499daf86848db13dcec901ec3a1adb
TheMagicNacho <justinhasanaccount@gmail.com>
2022-06-20 14:56:39 -0400

    terraform local dev env

--- Makefile
+++ Makefile
@@ -1,11 +1,29 @@
-server-dep:
+#!make
+include .env
+export $(shell sed 's/=.*//' .env)
+
+dep:
 	@cd server && go mod tidy
 
-server-build: server-dep
+binaries: dep
 	@cd server && go build -o ../bin/silmarillion-server
 
-server-img: server-build
+image: binaries
 	@docker build -t silmarillion-server .
 
-server-run: server-img
-	@docker run -d -p 8000:8000 --rm silmarillion-server
+logs: ## Logs the backend
+	@docker logs -f shepherd_server
+
+
+# Methods for developers
+container-run: image #Runs the server container with docker. Does not attach a database or proxy.
+	@docker run -d -p ${SERVER_PORT}:${SERVER_PORT}  --rm silmarillion-server
+
+
+up-dev: #Takes the most recent image and runs through terraform.
+	@echo "Did you build the most recent image?"
+	@cd terraform/local && terraform init && terraform apply -auto-approve
+
+down-dev:
+	@cd terraform/local && terraform destroy -auto-approve
+
---
+++ terraform/local/main.tf
@@ -0,0 +1,106 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 2.13.0"
+    }
+
+    postgresql = {
+      source  = "cyrilgdn/postgresql"
+      version = "~> 1.16.0"
+    }
+  }
+}
+
+provider "docker" {}
+
+resource "docker_image" "silmarillion-database" {
+  name         = "postgres:latest" # TODO: switch to specific version
+  keep_locally = false
+}
+
+resource "docker_container" "silmarillion-database" {
+  name  = "silmarillion-database"
+  image = docker_image.silmarillion-database.latest
+  ports {
+    internal = 5432
+    external = 5432
+  }
+  env = ["POSTGRES_USER=dev", "POSTGRES_PASSWORD=dev", "POSTGRES_DB=internal"]
+
+  networks_advanced {
+    name = "silmarillion-network"
+  }
+
+}
+
+## deploy the nginx proxy
+resource "docker_image" "silmarillion-nginx" {
+  name         = "nginx:latest" # TODO: switch to specific version
+  keep_locally = false
+}
+
+resource "docker_container" "silmarillion-nginx" {
+  image = docker_image.silmarillion-nginx.latest
+  name  = "silmarillion-nginx"
+  ports {
+    internal = 80
+    external = 80
+  }
+  networks_advanced {
+    name = "silmarillion-network"
+  }
+}
+
+
+# deploys the silmarillion-server. Assumes that you have built a docker image locally.
+resource "docker_image" "silmarillion-server" {
+  name         = "silmarillion-server:latest" 
+  keep_locally = true
+}
+
+resource "docker_container" "silmarillion-server" {
+  image    = docker_image.silmarillion-server.latest
+  name     = "silmarillion-server"
+  hostname = "silmarillion-server"
+  ports {
+    internal = 8000
+    external = 8000
+  }
+  networks_advanced {
+    name = "silmarillion-network"
+  }
+  depends_on = [
+    docker_network.private_network,
+    docker_container.silmarillion-database,
+  ]
+}
+
+
+resource "docker_network" "private_network" {
+  name = "silmarillion-network"
+}
+
+
+## Creates the database 
+## Commented out by using the environment variables shortcut found in the SQL module above
+provider "postgresql" {
+  host            = "localhost"
+  port            = 5432
+  database        = "internal"
+  username        = "dev"
+  password        = "dev"
+  sslmode         = "disable"
+  connect_timeout = 15
+}
+
+resource "postgresql_database" "silmarillion" {
+  name = "silmarillion"
+    owner             = "dev"
+  connection_limit  = 100
+  allow_connections = true
+  depends_on = [
+    docker_container.silmarillion-database
+  ]
+}
+
