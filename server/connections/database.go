98d3aaee0c426c793847b03e1eb443ae1f4fd48b
TheMagicNacho <justinhasanaccount@gmail.com>
2022-06-22 21:40:35 -0400

    fix connection bug

--- server/connections/database.go
+++ server/connections/database.go
@@ -15,10 +15,10 @@ func Database() {
 	dsn := "host=silmarillion-database port=5432 user=dev dbname=silmarillion password=dev sslmode=disable"
 
 	// connect to the database, if the connection fails try again
-	for {
+	for i := 0; i < 10; i++ {
 		dbScoped, err := gorm.Open(postgres.Open(dsn), &gorm.Config{Logger: logger.Default.LogMode(logger.Info)})
 		if err != nil {
-			fmt.Println(fmt.Errorf("Error connecting to database: %s", err))
+			fmt.Println(fmt.Errorf("error connecting to database: %s", err))
 			time.Sleep(time.Second * 3)
 			fmt.Println("Retrying connection...")
 		} else {
